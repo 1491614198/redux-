2572e79cf59e8b1ce20753d3db292b2fdfec7ffb
linzai <lin.zhou@newband.com>
2017-08-28 15:44:05 +0800

    this的五种方法

--- redux-practice/README.md
+++ redux-practice/README.md
@@ -1,5 +1,7 @@
-###说明###
+###  说明  ###
 
 npm i
 npm run build
 npm run start
+
+import {combineReducers} from 'redux-immutable'是可以让jsx都可以直接用state.get set等Immutable的使用
--- redux-practice/src/component/Task.jsx
+++ redux-practice/src/component/Task.jsx
@@ -4,6 +4,16 @@ import React,{Component} from 'react'
 class Task extends Component{
     constructor(props){
         super(props)
+        // this.change=this.change.bind(this)
+
+        // this.change = (e) =>{
+        //     e.preventDefault()
+        //         const b = e.target.value.trim();
+        //         if(!!b){
+        //             const {onSave} =this.props
+        //             onSave(b)
+        //         }
+        // }
     }
     change(e){
         e.preventDefault()
@@ -17,7 +27,10 @@ class Task extends Component{
         const {name} =this.props;
         return(
             <div>
-                <input type="text" placeholder="你的我的" onChange={this.change.bind(this)}/>
+                <input type="text" placeholder="你的我的" onChange={::this.change}/>
+                {/*<input type="text" placeholder="你的我的" onChange={this.change.bind(this)}/>*/}
+                {/*<input type="text" placeholder="你的我的" onChange={(e)=>{this.change(e)}}/>*/}
+                {/*／／this的6种语法*/}
                 {name}
             </div>
         )
